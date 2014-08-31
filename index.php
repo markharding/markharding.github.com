8e202fc1dc0600d124f8bc919b495ae8ea2a7d30
Mark Harding <mark@Marks-iMac.local>
2014-08-31 16:45:32 +0100

    email test

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 this is a test <?php echo 'and it works'; ?>
 
-<?php mail('mark@kramnorth.com', 'Just to test mark...', 'this is a test');
+<?php 
+
+use \google\appengine\api\mail\Message;
+
+try
+{
+  $message = new Message();
+  $message->setSender("mark@kramnorth.com");
+  $message->addTo("mark@kramnorth.com");
+  $message->setSubject("Example email");
+  $message->setTextBody("Hello, world!");
+ // $message->addAttachment('image.jpg', 'image data', $image_content_id);
+  $message->send();
+} catch (InvalidArgumentException $e) {
+  // ...
+}
